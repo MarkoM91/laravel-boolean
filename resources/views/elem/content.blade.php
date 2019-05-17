5506573ade41e5c719ca1d1d649a5dabc7f9ba88
MarkoM91 <markomedic91@gmail.com>
2019-05-17 11:44:58 +0200

    commit6

--- resources/views/elem/content.blade.php
+++ resources/views/elem/content.blade.php
@@ -10,25 +10,15 @@
     <div class="master">
       <div class="master-container">
           <div class="master-left">
-              <h2>Master Full Time</h2>
-              <p>Il corso full-time è proprio come una vera scuola con orari da rispettare, 5 giorni su 7.
-                Segui da casa tua le lezioni in diretta nella nostra aula virtuale</p>
+              <h2>Diventa sviluppatore web</h2>
+              <p>6 mesi di corso intensivo online in diretta per imparare da zero
+                 il mestiere di web developer e trovare un nuovo lavoro nelle aziende top del settore</p>
+              <input type="button" name="Scopri Il corso" value="Scopri Il Corso">
           </div>
           <div class="master-right">
+              <img src="https://www.boolean.careers/images/common/mac2x.png" alt="">
+          </div>
+      </div>
+  </div>
 
-          </div>
-      </div>
-  </div>
-  <div class="nostri-studenti">
-      <div class="nostri-studenti-container">
-          <p>I nostri studenti sono stati assunti da:</p>
-          <div class="icon">
-              <img src="https://www.boolean.careers/images/partners/facile_it.png">
-              <img src="https://www.boolean.careers/images/partners/oneday.png">
-              <img src="https://www.boolean.careers/images/partners/cayenne.png">
-              <img src="https://www.boolean.careers/images/partners/softec.png">
-              <img src="https://www.boolean.careers/images/partners/fabbrica_digitale.png">
-          </div>
-      </div>
-  </div>
 </div>
--- resources/views/ver1/home.blade.php
+++ resources/views/ver1/home.blade.php
@@ -36,47 +36,37 @@
           <div class="master">
             <div class="master-container">
                 <div class="master-left">
-                    <h2>Master Full Time</h2>
-                    <p>Il corso full-time è proprio come una vera scuola con orari da rispettare, 5 giorni su 7.
-                      Segui da casa tua le lezioni in diretta nella nostra aula virtuale</p>
+                    <h2>Diventa sviluppatore web</h2>
+                    <p>6 mesi di corso intensivo online in diretta per imparare da zero
+                       il mestiere di web developer e trovare un nuovo lavoro nelle aziende top del settore</p>
+                    <input type="button" name="Scopri Il corso">
                 </div>
                 <div class="master-right">
-
+                    <img src="https://www.boolean.careers/images/common/mac2x.png" alt="">
                 </div>
             </div>
         </div>
+    
       </div>
       <div class="footer">
           <div class="footer-container">
-              <div class="footer-up">
+
                 <div class="footer-left">
-                  <img src="https://www.boolean.careers/images/common/logo.png">
-              </div>
-                <div class="footer-right">
-                  <div class="footer-right-left">
-                      <span><a href="">ciao@boolean.careers</a>/span>
+                    <div class="footer-left-content">
+                      <span><a href="">ciao@boolean.careers</a></span>
                       <span><a href="#">02-40031288</a></span>
                       <span>+39 327-9578239</span>
                   </div>
-                  <div class="footer-right-center">
-                      <span>Privacy Policy</span>
-                      <span>Lavora con noi!</span>
-                  </div>
-                  <div class="footer-right-right">
+
+
+                  <div class="footer-right">
                       <span><i class="fab fa-facebook-square"></i></span>
                       <span><i class="fab fa-instagram"></i></span>
                       <span><i class="fab fa-whatsapp"></i></span>
                   </div>
-              </div>
-            </div>
-            <div class="footer-down">
-                <div class="footer-down-left">
-                    <p>Boolean** (my_new_job > my_old_job) // returns true</p>
-                </div>
-                <div class="footer-down-right">
-                    <p>Boolean SRL - Piazzale Giovanni dalle Bande Nere, 9 - 20146, Milano - P.IVA: 10214930967</p>
-                </div>
-            </div>
+
+
+
           </div>
       </div>
     </body>
