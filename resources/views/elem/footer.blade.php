b94a324a631d05a9551fc8bab6b545d627bf64cf
MarkoM91 <markomedic91@gmail.com>
2019-05-17 13:08:20 +0200

    commit13

--- resources/views/elem/footer.blade.php
+++ resources/views/elem/footer.blade.php
@@ -1,22 +1,15 @@
 
 <div class="footer">
     <div class="footer-container">
-
           <div class="footer-left">
-              
                 <span><a href="">ciao@boolean.careers</a></span>
                 <span><a href="#">02-40031288</a></span>
                 <span>+39 327-9578239</span>
             </div>
-
-
             <div class="footer-right">
                 <span><i class="fab fa-facebook-square"></i></span>
                 <span><i class="fab fa-instagram"></i></span>
                 <span><i class="fab fa-whatsapp"></i></span>
             </div>
-
-
-
     </div>
 </div>
