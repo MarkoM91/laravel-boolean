ca94ae3a434814f916acc4861c2ebbedfe99f5ff
MarkoM91 <markomedic91@gmail.com>
2019-05-17 14:26:50 +0200

    commit14

--- routes/web.php
+++ routes/web.php
@@ -3,21 +3,9 @@
 
 
 Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('elem/header', function () {
-    return view('elem.header');
-});
-Route::get('elem/content', function () {
-    return view('elem.content');
-});
-Route::get('elem/footer', function () {
-    return view('elem.footer');
-});
-Route::get('ver1', function () {
     return view('ver1.home');
 });
+
 Route::get('ver2', function () {
     return view('ver2.home');
 });
