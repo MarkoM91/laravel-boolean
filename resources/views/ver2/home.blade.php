ad53cf223e4268d6f69b38c59bcad291e3f07f30
MarkoM91 <markomedic91@gmail.com>
2019-05-19 19:35:22 +0200

    commit47

--- resources/views/ver2/home.blade.php
+++ resources/views/ver2/home.blade.php
@@ -4,7 +4,6 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-        <script type="{{ mix('js/script.js') }}"></script>
         <link rel="stylesheet" href="{{ mix('css/app.css') }}">
         <title>Laravel Header</title>
         </head>
